00c9e0251c55fbf34458a67e0357aac4266c1b28
castlez <castlez93@gmail.com>
2013-11-06 00:24:10 -0800

    trying to get deck to bring in from txt file having issues

--- mtg/src/ai/Deck.java
+++ mtg/src/ai/Deck.java
@@ -1,23 +1,37 @@
 package ai;
 import java.io.*;
+import java.util.*;
 
 public class Deck {
+	//grabs the file given and provides an input stream
 	private static InputStream getFileInputStream(String fileName)
 	{
-		InputStream	inputStream;
+		InputStream	is;
 		
 		try {
-			inputStream = new FileInputStream(new File(fileName));
-			}
-		catch (FileNotFoundException e) {			// no file with this name exists
+			is = new FileInputStream(new File(fileName));
+		}
+		catch (FileNotFoundException e) {			
+			is = null;
 			System.err.println(e.getMessage());
-			inputStream = null;
-			}
-		return inputStream;
+		}
+		return is;
 	}
-	InputStream in;
-	Deck(String deck){
+	
+	//vars
+	InputStream in;  //input stream
+	
+	ArrayList<Object> deck;
+	Scanner scan = new Scanner(in);  //TODO: not working 
+	
+	//constructor
+	public Deck(String deck){
 		in = getFileInputStream(deck);
+		while(scan.hasNext()){
+			System.out.println(scan.next().toString());
+			
+		}
 	}
+
 	
 }
--- mtg/src/main/Game.java
+++ mtg/src/main/Game.java
@@ -1,6 +1,8 @@
 package main;
-
+import ai.*;
 
 public class Game {
-	
+	public static void main(String [] args){
+		Deck op = new Deck(args[0]);
+	}
 }
