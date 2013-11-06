3ed7b1f789744fb92191a7e75c1715afca1e9bc2
castlez <castlez93@gmail.com>
2013-11-05 23:47:34 -0800

    add input stream function to Deck

--- mtg/src/ai/Deck.java
+++ mtg/src/ai/Deck.java
@@ -2,6 +2,19 @@ package ai;
 import java.io.*;
 
 public class Deck {
+	private static InputStream getFileInputStream(String fileName)
+	{
+		InputStream	inputStream;
+		
+		try {
+			inputStream = new FileInputStream(new File(fileName));
+			}
+		catch (FileNotFoundException e) {			// no file with this name exists
+			System.err.println(e.getMessage());
+			inputStream = null;
+			}
+		return inputStream;
+	}
 	InputStream in;
 	Deck(String deck){
 		in = getFileInputStream(deck);
