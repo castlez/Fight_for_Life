d03d90d6995d4f0e33e0518a97b9c8f92ded6af4
castlez <castlez93@gmail.com>
2013-11-06 21:32:17 -0800

    add to Deck class: 	toString 	addCard
    fixed constructor (now working)

--- mtg/src/ai/Deck.java
+++ mtg/src/ai/Deck.java
@@ -25,17 +25,46 @@ public class Deck {
 	Scanner scan;  //TODO: not working 
 	
 	//constructor
-	public Deck(String deck){
-		in = getFileInputStream(deck);
+	public Deck(String deckType){
+		in = getFileInputStream(deckType);
 		scan = new Scanner(in);
+		
+		deck = new ArrayList<Object>();
+		
 		String next;
+		Integer count;
+		
 		while(scan.hasNext()){
 			next = scan.next();
 			System.out.println(next);
+			count = 1;
 			if(next.startsWith("/")){
-				System.out.println("found a comment");
 				continue;
+			}
+			else if (next.matches("\\d+")){
+				count = Integer.parseInt(next);
+			}	
+			else{
+				for(int i = 0; i<count;i++){
+					addCard(next);
+				}
 			}
 		}
 	}	
+	
+	public void addCard(String card){
+		@SuppressWarnings("rawtypes")
+		Class toAdd;
+		try {
+			toAdd = Class.forName("goblins." + card); //make generic when it works
+			deck.add(toAdd);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String toString(){
+		return deck.toString();
+		
+	}
 }
--- mtg/src/main/Game.java
+++ mtg/src/main/Game.java
@@ -4,5 +4,6 @@ import ai.*;
 public class Game {
 	public static void main(String [] args){
 		Deck op = new Deck("goblins.txt");
+		System.out.println(op.toString());
 	}
 }
